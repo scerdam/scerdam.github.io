2205d7308113567c8e8ea2a414c9457a112e2a82
scerdam <sebastian.cerdam@usach.cl>
2017-07-07 20:15:44 -0400

    animaciones wow

--- assets/js/gototopv1.js
+++ assets/js/gototopv1.js
@@ -1,42 +1,44 @@
+new WOW().init();
+
 // Only enable if the document has a long scroll bar
 // Note the window height + offset
-if ( ($(window).height() + 100) < $(document).height() ) {
-    $('#top-link-block').removeClass('hidden').affix({
-        // how far to scroll down before link "slides" into view
-        offset: {top:100}
-    });
-}
+// if ( ($(window).height() + 100) < $(document).height() ) {
+//     $('#top-link-block').removeClass('hidden').affix({
+//         // how far to scroll down before link "slides" into view
+//         offset: {top:100}
+//     });
+// }
+//
+// $(function () {
+//
+//     "use strict";
+//
+//     var $bgobj = $(".ha-bg-parallax"); // assigning the object
+//
+//     $(window).on("scroll", function () {
+//
+//         var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+//
+//         // Put together our final background position
+//
+//         var coords = '100% ' + yPos + 'px';
+//
+//         // Move the background
+//
+//         $bgobj.css({ backgroundPosition: coords });
+//
+//     });
+//         $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
+// 		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
+// 		$(this).addClass('selected');
+// 		$(this).find('input[type="radio"]').prop("checked", true);
+//
+// 	});
+//
+// });
 
-$(function () {
-
-    "use strict";
-
-    var $bgobj = $(".ha-bg-parallax"); // assigning the object
-
-    $(window).on("scroll", function () {
-
-        var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
-
-        // Put together our final background position
-
-        var coords = '100% ' + yPos + 'px';
-
-        // Move the background
-
-        $bgobj.css({ backgroundPosition: coords });
-
-    });
-        $('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
-		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
-		$(this).addClass('selected');
-		$(this).find('input[type="radio"]').prop("checked", true);
-		
-	});
-
-});
 
 
- 
 $('#item0').on('shown.bs.collapse', function () {
    $("#foo").removeClass("fa fa-chevron-down").addClass("fa fa-chevron-up");
 });
@@ -110,6 +112,3 @@ $('#promo3').on('shown.bs.collapse', function () {
 $('#promo3').on('hidden.bs.collapse', function () {
    $("#foo8").removeClass("fa fa-chevron-up").addClass("fa fa-chevron-down");
 });
-
-
-
